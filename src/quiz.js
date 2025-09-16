372e90d729f3370a1e9ba6a0af702549db346b04
Danish Shaikh <danishshk0399@gmail.com>
2025-09-16 23:57:56 +0530

    feat: add initial App and Quiz components with rendering setup

---
+++ src/quiz.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Quiz from "./components/Quiz";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("quiz-root"));
+root.render(
+  <React.StrictMode>
+    <Quiz />
+  </React.StrictMode>
+);
