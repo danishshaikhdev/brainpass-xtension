dabfde06ed00ba1be2b425eef0bd45a6dd762647
Danish Shaikh <danishshk0399@gmail.com>
2025-09-17 14:09:58 +0530

    fix: update import path for App component in popup.js and replace Quiz with App in quiz.js

--- src/quiz.js
+++ src/quiz.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Quiz from "./components/Quiz";
+import App from "./components/App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("quiz-root"));
 root.render(
   <React.StrictMode>
-    <Quiz />
+    <App />
   </React.StrictMode>
 );
